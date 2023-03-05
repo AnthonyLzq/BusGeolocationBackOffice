2e20f21554382d688390a84897380ffe6bc1b8a6
AnthonyLzq <sluzquinosa@uni.pe>
2023-03-05 02:29:37 -0500

    feat: making the component capable of rendering multiple bus routes

--- app/components/Map.client.tsx
+++ app/components/Map.client.tsx
@@ -9,6 +9,21 @@ import Bus from '../static/images/bus.png'
 
 type Point = [lat: number, lng: number]
 
+type AllClientsData = {
+  [key: string]: {
+    [key: string]: {
+      [key: string]: {
+        date: string
+        position: {
+          lat: number
+          lng: number
+        }
+        state: 'on' | 'off'
+      }
+    }
+  }
+}
+
 type Props = {
   height: string
   width: string
@@ -18,23 +33,54 @@ const BusIcon = new Icon({
   iconUrl: Bus,
   iconSize: [24, 24]
 })
-const initialPosition: LatLngTuple = [-12.025560850179993, -77.05343209496314]
+const initialPosition: LatLngTuple = [-12.084489222242723, -76.97945124985259]
+
+const randomHexColor = () => {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16)
+
+  return `#${n.slice(0, 6).toUpperCase()}`
+}
 
 const Map: FC<Props> = props => {
-  const [polyline, setPolyline] = useState<LatLngTuple[]>([])
+  const [buses, setBuses] = useState<string[]>([])
+  const [polylines, setPolylines] = useState<LatLngTuple[][]>([[]])
+  const [colors, setColors] = useState<string[]>([])
+  console.log('🚀 ~ file: Map.client.tsx:51 ~ colors:', colors)
   const { height, width } = props
 
   useEffect(() => {
     const socket = io(ENV.WS_URL, {
       reconnectionDelayMax: 10000,
-      query: {
-        id: '1'
-      },
       transports: ['websocket', 'polling', 'flashsocket']
     })
 
-    socket.on('bus/position', (message: Point) => {
-      setPolyline(prevPolyline => prevPolyline.concat([message]))
+    socket.on('initialData', (message: AllClientsData) => {
+      const newBuses: string[] = []
+
+      for (const client in message)
+        for (const line in message[client])
+          for (const bus in message[client][line]) newBuses.push(bus)
+
+      setBuses(newBuses)
+
+      const newColors = Array.from({ length: newBuses.length }, () =>
+        randomHexColor()
+      )
+
+      setColors(newColors)
+      newBuses.forEach((bus, index) => {
+        socket.on(`${bus}/position`, (message: Point) => {
+          setPolylines(prevState => {
+            const newState = prevState.map(polyline => [...new Set(polyline)])
+
+            if (!newState[index]) newState[index] = []
+
+            newState[index].push(message)
+
+            return newState
+          })
+        })
+      })
     })
 
     return () => {
@@ -59,19 +105,25 @@ const Map: FC<Props> = props => {
           borderRadius: '16px'
         }}
         center={initialPosition}
-        zoom={16}
+        zoom={12}
         scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        {polyline.length > 0 ? (
+        {polylines[0].length > 0 ? (
           <>
-            <Marker position={polyline[polyline.length - 1]} icon={BusIcon}>
-              <Popup>UNI de mierda</Popup>
-            </Marker>
-            <Polyline positions={polyline} />
+            {polylines.map((polyline, index) => (
+              <Marker
+                key={buses[index]}
+                position={polyline[polyline.length - 1]}
+                icon={BusIcon}
+              >
+                <Popup>Bus: {buses[index]}</Popup>
+                <Polyline positions={polylines[index]} color={colors[index]} />
+              </Marker>
+            ))}
           </>
         ) : null}
       </MapContainer>
